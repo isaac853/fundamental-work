7aede52cc14a602eef5c384fdafd259630edeb2d
Isaac S <8111@balcarras.gloucs.sch.uk>
2025-10-07 10:42:47 +0000

    merge sort working

--- sorters/mergeSort.py
+++ sorters/mergeSort.py
@@ -3,25 +3,31 @@ data = [random.randint(0,100) for i in range(11)]
 print(data)
 
 def mergesort(data):
-    if len(data) > 1:
+    if len(data) == 1:
+        return data
+    
+    
+    else:
+
         middle = len(data)//2
         left = mergesort(data[:middle])
         right = mergesort(data[middle:])
         newlist = []
+
         while len(left) > 0 and len(right) > 0:
             if left[0] < right[0]:
-                newlist.append(left[0])
-                del left[0]
+                newlist.append(left.pop(0))
+
             else:
-                newlist.append(right[0])
-                del right[0]
+                newlist.append(right.pop(0))
+
+
 
         if len(left) != 0:
-            newlist.append(left)
+            newlist += left
         else:
-            newlist.append(right)
+            newlist += right
         return newlist
-    else:
-        return data
+
 
 print(mergesort(data))
